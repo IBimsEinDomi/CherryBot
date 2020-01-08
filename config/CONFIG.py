6e10a710b598870ee9cc8764b85a8e1388e91187
Dominik Büttner <50876016+IBimsEinMystery@users.noreply.github.com>
2020-01-08 12:04:33 +0100

    Addet SupportList

--- config/CONFIG.py
+++ config/CONFIG.py
@@ -1,6 +1,6 @@
 import discord
 
-TOKEN = "DEIN-TOKEN"
+TOKEN = "NjU3MTY5NzY5NTg5Mzc0OTc3.XftadQ.2Rwfpz3ni9gq3FkDPpDdLGEwvyc"
 PREFIX = "&"
 serverLicense = "Keine Lizenz"
 clientLogout = False
@@ -8,4 +8,7 @@ AllowedServer = [
             479691134482776066,
             453203550579982347,
             660091191752261662
+                 ]
+SupportLicenseServer = [
+            479691134482776066
                  ]
