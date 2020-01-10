9703d7e5867e8ee5ec22aea490f84f103dbca971
Dominik Büttner <50876016+IBimsEinMystery@users.noreply.github.com>
2020-01-10 12:30:32 +0100

    Update GAMES.py

--- config/GAMES.py
+++ config/GAMES.py
@@ -26,13 +26,14 @@ import discord
 
 __games__ = [
     (discord.ActivityType.playing, 'mit Python'),
-    (discord.ActivityType.watching, 'über {guilds} Server'),
-    (discord.ActivityType.watching, 'über {members} Mitglieder'),
+    (discord.ActivityType.watching, 'auf {guilds} Server'),
+    (discord.ActivityType.watching, 'auf {members} Mitglieder'),
     (discord.ActivityType.listening, '&bot-info')
 ]
 __gamesTimer__ = [
     2 * 60,
     1 * 35,
     2 * 27,
-    3 * 23
+    3 * 23,
+    5 * 9
 ]
